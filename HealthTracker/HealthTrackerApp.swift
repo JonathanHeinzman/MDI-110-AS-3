1b7fb0edbf3224543ff69d8e8d74c23749ec8af0
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-07-29 18:38:46 -0600

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Jonathan Heinzman on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
